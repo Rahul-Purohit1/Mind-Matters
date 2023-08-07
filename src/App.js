ae0111250d5ed36c3ceb8884998b06537a30adb0
Mayank Sinhal <sinhal.mayank.mk@gmail.com>
2023-08-07 22:11:43 +0530

    added footer just review it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
